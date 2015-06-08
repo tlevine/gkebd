e546cb214ea3b3f6ce805826f178bb6c5a433188
Thomas Levine <_@thomaslevine.com>
2015-06-08 09:12:45 +0000

    yay

--- gkebd/main.py
+++ gkebd/main.py
@@ -1,4 +1,5 @@
 import sys
+from concurrent.futures import ThreadPoolExecutor
 
 from vlermv import cache
 import requests
@@ -9,17 +10,21 @@ get = cache('~/.gkebd', cache_exceptions = True)(requests.get)
 
 def gkebd():
     url = 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_Sveriges_kommuner'
-    for kommun in parse.kommuner(get(url)):
-        for url in parse.hemsida(kommun):
-            try:
-                r = get(url, timeout = 10)
-            except requests.exceptions.ConnectionError:
-                pass
-            else:
-                yield kommun, parse.tracking(r)
-                break
+    with ThreadPoolExecutor(20) as e:
+        for row in e.map(gkebd_one, parse.kommuner(get(url))):
+            if row != None:
+                yield row
+
+def gkebd_one(kommun):
+    for url in parse.hemsida(kommun):
+        try:
+            r = get(url, timeout = 10)
+        except requests.exceptions.ConnectionError:
+            pass
         else:
-             sys.stderr.write('Could not find a page for %s\n' % kommun)
+            return kommun, parse.tracking(r)
+    else:
+         sys.stderr.write('Could not find a page for %s\n' % kommun)
 
 def cli():
     import csv
