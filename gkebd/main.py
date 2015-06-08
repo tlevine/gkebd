3532b3a080d11a429f23ce4efdc6d521afe029c7
Thomas Levine <_@thomaslevine.com>
2015-06-08 08:59:37 +0000

    woo

--- gkebd/main.py
+++ gkebd/main.py
@@ -1,16 +1,30 @@
+import sys
+
 from vlermv import cache
+import requests
 
 from . import parse
-from .check import check
 
-get = cache('~/.gkebd')(requests.get)
+get = cache('~/.gkebd', cache_exceptions = True)(requests.get)
 
 def gkebd():
     url = 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_Sveriges_kommuner'
     for kommun in parse.kommuner(get(url)):
-        yield parse.tracking(get(parse.hemsida(kommun)))
+        for url in parse.hemsida(kommun):
+            try:
+                r = get(url, timeout = 10)
+            except requests.exceptions.ConnectionError:
+                pass
+            else:
+                yield kommun, parse.tracking(r)
+                break
+        else:
+             sys.stderr.write('Could not find a page for %s\n' % kommun)
 
 def cli():
-    import csv, sys
-    for row in main():
-        print(row)
+    import csv
+    w = csv.writer(sys.stdout)
+    w.writerow(('kommun', 'third.party'))
+    for kommun, trackers in gkebd():
+        for tracker in trackers:
+            w.writerow((kommun, tracker))
--- gkebd/parse.py
+++ gkebd/parse.py
@@ -1,4 +1,5 @@
 import re
+from urllib.parse import urlparse
 
 from lxml.html import fromstring
 from unidecode import unidecode
@@ -8,14 +9,25 @@ def hemsida(kommun):
     >>> hemsida('Vetlanda kommun')
     'http://vetlanda.se'
     '''
-    m = re.match(r'(.*)s? kommun$', kommun)
+    m = re.match(r'^(.+[^s])(s?) kommun$', kommun)
     if not m:
         assert False, kommun
-    return 'http://%s.se' % unidecode(m.group(1).lower())
+    x = unidecode(m.group(1).lower())
+    for scheme in ['http', 'https']:
+        for prefix in ['', 'www.']:
+            yield '%s://%s%s.se' % (scheme, prefix, x)
+            if m.group(2) == 's':
+                yield '%s://%s%ss.se' % (scheme, prefix, x)
 
 def kommuner(response):
-    html = fromstring(response)
-    return filter(lambda href: href.endswith(' kommun'), html.xpath('//a/@href'))
+    html = fromstring(response.content)
+    return filter(lambda text: text.endswith(' kommun'), html.xpath('//a/text()'))
 
 def tracking(response):
-    pass
+    html = fromstring(response.content)
+    html.make_links_absolute(response.url)
+    for src in html.xpath('//script/@src'):
+        p = urlparse(src)
+        url = '%s://%s' % (p.scheme, p.netloc)
+        if url != response.url.strip('/'):
+            yield url
