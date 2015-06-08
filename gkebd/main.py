af39a42dc035d7781c01cb9691a729a28c765ade
Thomas Levine <_@thomaslevine.com>
2015-06-08 08:31:04 +0000

    j

--- gkebd/main.py
+++ gkebd/main.py
@@ -3,9 +3,9 @@ from vlermv import cache
 from . import parse
 from .check import check
 
-get = cache('~/.sbrösb')(requests.get)
+get = cache('~/.gkebd')(requests.get)
 
-def sbrör():
+def gkebd():
     url = 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_Sveriges_kommuner'
     for kommun in parse.kommuner(get(url)):
         yield check(get(parse.hemsida(kommun)))
