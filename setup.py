1e870abd142eae09c38bad5234915eafded58403
Thomas Levine <_@thomaslevine.com>
2015-06-08 09:14:04 +0000

    setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gkebd',
+      author='Thomas Levine',
+      author_email='_@thomaslevine.com',
+      description='Look for (illegal) third-party trackers on Swedish government websites.',
+      url='https://dada.pink/gkebd/',
+      packages=['gkebd'],
+      install_requires = [],
+      version='0.0.1',
+      license='AGPL',
+      entry_points = {'console_scripts': ['gkebd = gkebd.main:cli']},
+)
