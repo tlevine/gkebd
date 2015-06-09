34364e8d54388d923d38641193e7bca28f0e8df6
Thomas Levine <_@thomaslevine.com>
2015-06-09 17:55:10 +0000

    woo

--- gkebd/main.py
+++ gkebd/main.py
@@ -1,4 +1,6 @@
-import sys
+import os, sys
+import subprocess, json, tempfile
+from collections import OrderedDict
 from concurrent.futures import ThreadPoolExecutor
 
 from vlermv import cache
@@ -10,11 +12,19 @@ from . import parse
 def get(url, *args, **kwargs):
     return requests.get(url, *args, headers = { 'user-agent': 'https://pypi.python.org/pypi/gkebd'})
 
+@cache('~/.gkebd-cookies', serializer = json)
+def check_cookies(url):
+    with tempfile.NamedTemporaryFile(mode = 'r+') as tmp:
+        subprocess.call(['check-cookies.js', tmp.name, url])
+        tmp.file.seek(0)
+        cookies = json.load(tmp.file)
+    return cookies
+
 def startsidor():
     url = 'https://sv.wikipedia.org/wiki/Lista_%C3%B6ver_Sveriges_kommuner'
     with ThreadPoolExecutor(20) as e:
         for startsida, response in e.map(hitta_startsida, parse.kommuner(get(url))):
-            if startsida, response != None:
+            if response != None:
                 yield startsida, response
 
 def hitta_startsida(kommun):
@@ -28,32 +38,43 @@ def hitta_startsida(kommun):
         else:
             return kommun, url
     else:
-         sys.stderr.write('Could not find a page for %s\n' % kommun)
+        sys.stderr.write('Could not find a page for %s\n' % kommun)
+        return None, None
 
 def cli():
-    import subprocess, json, tempfile
     import dataset
 
     fn = 'gkebd.sqlite'
     if os.path.exists(fn):
         os.remove(fn)
     db = dataset.connect('sqlite:///' + fn)
+    db.engine.execute('CREATE TABLE startsida (kommun TEXT, UNIQUE(kommun))')
+    db.engine.execute('''
+CREATE TABLE kaka (
+  kommun TEXT NOT NULL,
+  name TEXT NOT NULL,
+  value TEXT NOT NULL,
+  domain TEXT NOT NULL,
+  path TEXT NOT NULL,
+  httponly INTEGER NOT NULL,
+  secure INTEGER NOT NULL,
+  expires TEXT NOT NULL,
+  expiry TEXT NOT NULL,
 
-    tabell = {
-        'startsida': db.get_table('startsidor', primary_id = 'kommun'),
-        'skript': db.get_table('skript'),
-        'kaka': db.get_table('kaka', primary_id = ('kommun', 'name')),
-    }
+  UNIQUE(kommun, name)
+)''')
 
-    for kommun, startsida in startsidor():
-        db['startsida'].insert({'kommun': kommun, 'startsida': startsida})
+    with ThreadPoolExecutor(20) as e:
+        for kommun, startsida in startsidor():
+            future = e.submit(gkebd, db, kommun, startsida)
 
-        for thirdparty in parse.tracking(get(startsida)):
-            db['skript'].insert({'kommun': kommun, 'thirdparty': thirdparty})
+def gkebd(db, kommun, startsida):
+    db['startsida'].insert({'kommun': kommun, 'startsida': startsida})
 
-        with tempfile.NamedTemporaryFile() as tmp:
-            subprocess.call(['check-cookies.js', tmp.name, startsida])
-            tmp.file.seek(0)
-            for kaka in json.load(tmp.file):
-                kaka['kommun'] = kommun
-                db['kaka'].insert(kaka)
+    for thirdparty in parse.tracking(get(startsida)):
+        db['skript'].insert({'kommun': kommun, 'thirdparty': thirdparty})
+
+    kakor = check_cookies(startsida)
+    for kaka in kakor:
+        kaka['kommun'] = kommun
+    db['kaka'].insert_many(kakor)
