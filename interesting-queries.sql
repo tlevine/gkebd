8e384bf1503362f54aa4acec9b020fea5ed87ca4
Thomas Levine <_@thomaslevine.com>
2015-06-09 21:08:03 +0000

    another query

--- interesting-queries.sql
+++ interesting-queries.sql
@@ -2,17 +2,17 @@
 create view hur_många_skript as
 select kommun, count(*) as 'skript'
 from skript
-group by kommun
+group by kommun;
 
 create view hur_många_kakor as
 select kommun, count(*) as 'kakor'
 from kaka
-group by kommun
+group by kommun;
 
 select
   hur_många_skript.kommun,
   hur_många_skript.skript,
-  hur_många_kakor.kakor,
+  hur_många_kakor.kakor
 from hur_många_skript
 join hur_många_kakor
 on hur_många_skript.kommun = hur_många_kakor.kommun
@@ -24,4 +24,6 @@ from kaka
 group by kommun
 order by max(expires);
 
--- 
+-- Varifrån kommer kakor?
+select ltrim(replace(domain, 'www.', ''), '.') AS 'domain', count(*)
+from kaka group by 1 order by 2;
