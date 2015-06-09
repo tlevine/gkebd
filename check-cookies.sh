239ccc04c45118f1694ef63d9661e403e61f976a
Thomas Levine <_@thomaslevine.com>
2015-06-09 15:28:19 +0000

    more elinks options

--- check-cookies.sh
+++ check-cookies.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+set -e
+
 url="$1"
 domain=$(echo "$url" | cut -d/ -f3)
-echo $domain
+basedir=$($PWD/elinks/$(date --rfc-3339 day))
+
+elinks -config-dir "$basedir/$domain" -no-connect 1 \
+  -eval 'set ecmascript.enable = 1' \
+  -eval 'set ecmascript.ignore_noscript = 1' \
+  "$url"
