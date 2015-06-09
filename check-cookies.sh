7c92d963ac1bb27f9949f9703eb3bac619f2c8b2
Thomas Levine <_@thomaslevine.com>
2015-06-09 15:48:24 +0000

    blegh

--- check-cookies.sh
+++ check-cookies.sh
@@ -3,9 +3,12 @@ set -e
 
 url="$1"
 domain=$(echo "$url" | cut -d/ -f3)
-basedir=$($PWD/elinks/$(date --rfc-3339 day))
 
-elinks -config-dir "$basedir/$domain" -no-connect 1 \
+configdir=$(echo "$PWD"/elinks/$(date --rfc-3339 date)/"$domain")
+echo $configdir
+mkdir -p "$configdir"
+
+elinks -config-dir "$configdir" -no-connect 1 -touch-files 1 \
   -eval 'set ecmascript.enable = 1' \
   -eval 'set ecmascript.ignore_noscript = 1' \
   "$url"
