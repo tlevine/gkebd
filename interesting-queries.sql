e99968dba5402b91ca35ec2b56d2ff7ed28f76ce
Thomas Levine <_@thomaslevine.com>
2015-06-09 21:21:23 +0000

    j

--- interesting-queries.sql
+++ interesting-queries.sql
@@ -1,18 +1,18 @@
--- Varifrån kommer kakorna?
-select ltrim(replace(domain, 'www.', ''), '.') AS 'domain', count(*)
+create view if not exists varifrån_kommer_kakorna as
+select ltrim(replace(domain, 'www.', ''), '.') as 'domain', count(*)
 from kaka group by 1 order by 2;
 
--- Hur många skript och kakor?
-create view hur_många_skript as
+create view if not exists hur_många_skript as
 select kommun, count(*) as 'skript'
 from skript
 group by kommun;
 
-create view hur_många_kakor as
+create view if not exists hur_många_kakor as
 select kommun, count(*) as 'kakor'
 from kaka
 group by kommun;
 
+create view if not exists hur_många_skript_och_kakor as
 select
   hur_många_skript.kommun,
   hur_många_skript.skript,
@@ -22,7 +22,7 @@ join hur_många_kakor
 on hur_många_skript.kommun = hur_många_kakor.kommun
 order by skript, kakor;
 
--- Datum
+create view if not exists lång_kakor as
 select kommun, max(expires)
 from kaka
 group by kommun
