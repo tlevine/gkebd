d7840ac934c95e398431917ab5d6aae852f2787e
Thomas Levine <_@thomaslevine.com>
2015-06-09 20:58:13 +0000

    interesting-queries.sql

---
+++ interesting-queries.sql
@@ -0,0 +1,27 @@
+-- Hur många
+create view hur_många_skript as
+select kommun, count(*) as 'skript'
+from skript
+group by kommun
+
+create view hur_många_kakor as
+select kommun, count(*) as 'kakor'
+from kaka
+group by kommun
+
+select
+  hur_många_skript.kommun,
+  hur_många_skript.skript,
+  hur_många_kakor.kakor,
+from hur_många_skript
+join hur_många_kakor
+on hur_många_skript.kommun = hur_många_kakor.kommun
+order by skript, kakor;
+
+-- Datum
+select kommun, max(expires)
+from kaka
+group by kommun
+order by max(expires);
+
+-- 
